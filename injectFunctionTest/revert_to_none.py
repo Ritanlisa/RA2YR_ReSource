c105bbf06769717d1cc670d7723c993d9d97392a
Ritanlisa <jack1122334455@126.com>
2026-06-07 18:02:02 +0800

    Clean up test scripts

--- injectFunctionTest/revert_to_none.py
+++
@@ -1,8 +0,0 @@
-import io
-file = r'D:\RA2YR_ReSource\src\render\surface.cpp'
-with io.open(file, 'r', encoding='utf-8') as f:
-    c = f.read()
-c = c.replace('"Capture"', '"None"')
-with io.open(file, 'w', encoding='utf-8') as f:
-    f.write(c)
-print('Done')
